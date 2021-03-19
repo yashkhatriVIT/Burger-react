60f465c33cef1095d5c5518f5329e45e88b5f1ec
yashkhatriVIT <yash.khatri@vitstudent.ac.in>
2021-03-19 22:18:37 +0530

    Initial Commit

---
+++ src/components/Navigation/NavigationItems/Navigation.test.js
@@ -0,0 +1,14 @@
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItem from './NavigationItem/NavigationItem';
+import NavigationItems from './NavigationItems';
+
+configure({adapter: new Adapter()});
+
+
+describe('<NavigationItems />', () => {
+    it('should render two <Navigation if not authenticated', ()=>{
+        const wrapper = shallow(<NavigationItems/>);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
+import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose: null;
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer, 
+    order: orderReducer,
+    auth: authReducer
+});
+
+// const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer,applyMiddleware(thunk));
+
+
+// const store = createStore(
+//     burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   );
+const app = (
+    <Provider store = {store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render( app, document.getElementById( 'root' ) );
+registerServiceWorker();
+
+ 
